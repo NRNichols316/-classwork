a0ef7f2072ecb1cd3f0d7f7e13cb1fcda9fbd965
Pookie <Pookie>
2018-10-09 16:56:50 -0400

    adding second part of quiz

---
+++ Quiz1/HolidayChallenge.js
@@ -0,0 +1,2 @@
+let answer = ((100000 * .02) + (100000 * .04));
+console.log("I will raise " + answer + " for charity this year");
---
+++ Quiz1/StarwarsRuntimeHelper.js
@@ -0,0 +1,2 @@
+let answer = (136%60);
+console.log("Minutes past the 2nd hour: " + answer + " mins");
